0b319f28951ad12d4ca8b49d4f3181ee656cda00
Mario5999 <243716@ids.upchiapas.edu.mx>
2025-11-28 20:15:12 -0600

    Creacion de la vista de login

---
+++ Feats-UP/src/app/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true
+})
+export class LoginComponent {
+  // Aquí puedes agregar lógica de autenticación más adelante
+}
