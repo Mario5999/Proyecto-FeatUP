286b6727338ce9dfdeb19c5e61a3e640ece20d9c
Mario5999 <243716@ids.upchiapas.edu.mx>
2025-11-28 22:16:46 -0600

    Vista register, agrego de alertas cuando los campos estan vacios y quiere registrarse

--- Feats-UP/src/app/Registro/register.component.ts
+++ Feats-UP/src/app/Registro/register.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  // Aquí puedes agregar lógica de registro más adelante
+  form; // declaramos la propiedad
+
+  constructor(private fb: FormBuilder) {
+    // inicializamos aquí, cuando fb ya existe
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      fecha: ['', Validators.required],
+      celular: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]]
+    });
+  }
+
+  crearCuenta() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('Formulario válido:', this.form.value);
+  }
 }
