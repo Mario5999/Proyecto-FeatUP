d8775de25f545e246173039b8edd418cf9b292c5
Mario5999 <243716@ids.upchiapas.edu.mx>
2025-11-28 21:45:26 -0600

    Vista login, agrego de alertas cuando los campos estan vacios y quiere iniciar sesion

--- Feats-UP/src/app/Inicio de sesion/login.component.ts
+++ Feats-UP/src/app/Inicio de sesion/login.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent {
-  // Aquí puedes agregar lógica de autenticación más adelante
+  form;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      nombre: ['', Validators.required],
+      apellidos: ['', Validators.required]
+    });
+  }
+
+  ingresar() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('Formulario válido:', this.form.value);
+  }
 }
